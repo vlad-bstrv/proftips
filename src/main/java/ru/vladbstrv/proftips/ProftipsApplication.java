d72804cb57bb8506938f062db0fef1eda816d56a
vladbstrv <keocer@gmail.com>
2023-08-30 12:37:56 +0300

    Initial commit

---
+++ src/main/java/ru/vladbstrv/proftips/ProftipsApplication.java
@@ -0,0 +1,13 @@
+package ru.vladbstrv.proftips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProftipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProftipsApplication.class, args);
+    }
+
+}
